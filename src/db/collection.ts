ae1525e1eee11bba8f8efa4b03962052d44c020a
Willian Rodrigues <w.almeida.w@outlook.com>
2025-04-10 09:49:38 -0300

    feat: informações de coleções

---
+++ src/db/collection.ts
@@ -0,0 +1,40 @@
+const database = [
+    {
+        "id":1,
+        "colecao": "0001-DominacaoGeneticaA1",
+        "minId": 1,
+        "maxId": 286
+    },
+    {
+        "id":2,
+        "colecao": "0002-PromoA",
+        "minId": 1,
+        "maxId": 59
+    },
+    {
+        "id":3,
+        "colecao": "0003-IlhaMitica",
+        "minId": 1,
+        "maxId": 86
+    },
+    {
+        "id":4,
+        "colecao": "0004-EmbateTempoEspaco",
+        "minId": 1,
+        "maxId": 207
+    },
+    {
+        "id":5,
+        "colecao": "0005-LuzTriunfante",
+        "minId": 1,
+        "maxId": 96
+    },
+    {
+        "id":6,
+        "colecao": "0006-FestivalBrilhante",
+        "minId": 1,
+        "maxId": 111
+    }
+];
+
+export default database;
