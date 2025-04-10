4f946a383482b119fdc8ac78ee9cb3e0e363a0e8
Willian Rodrigues <w.almeida.w@outlook.com>
2025-04-10 09:50:48 -0300

    feat: Rota com POST para compactar os dados e um GET com dados de exemplo

---
+++ src/app/data/route.ts
@@ -0,0 +1,47 @@
+import { minifyData } from "@/lib/utils";
+import { TData } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET() {
+    const data: TData = {
+        name: "John Doe",
+        icon: "123",
+        id: "12345",
+        have: [
+            { colecao: "0006-FestivalBrilhante", id: "4" },
+            { colecao: "0006-FestivalBrilhante", id: "88" },
+            { colecao: "0006-FestivalBrilhante", id: "81" },
+            { colecao: "0006-FestivalBrilhante", id: "72" },
+            { colecao: "0006-FestivalBrilhante", id: "50" },
+            { colecao: "0006-FestivalBrilhante", id: "52" },
+            { colecao: "0006-FestivalBrilhante", id: "42" },
+            { colecao: "0006-FestivalBrilhante", id: "42" },
+            { colecao: "0006-FestivalBrilhante", id: "42" },
+        ],
+        want: [
+            { colecao: "0006-FestivalBrilhante", id: "74" },
+            { colecao: "0006-FestivalBrilhante", id: "75" },
+            { colecao: "0006-FestivalBrilhante", id: "75" },
+        ],
+    };
+    return new Response(btoa(JSON.stringify(data)), {
+        status: 200,
+    });
+}
+
+export async function POST(request: NextRequest) {
+    const data = (await request.json()) as TData;
+    if (!data) {
+        return new Response("No data", { status: 400 });
+    }
+    const minify = minifyData(data);
+    const encodedData = btoa(JSON.stringify(minify));
+
+    return NextResponse.json(
+        {
+            minify,
+            encodedData,
+        },
+        { status: 200 }
+    );
+}
