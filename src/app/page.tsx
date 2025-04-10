58096ae109b9f7d8c7f288d8e17a0ba2b54fa5ad
Willian Rodrigues <w.almeida.w@outlook.com>
2025-04-10 09:51:50 -0300

    docs: Página de explicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,63 @@
-import Image from "next/image";
+import data from "@/db/collection";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+export default function Page(){
+    return <div className="container">
+        <h1>Coleções suportadas</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Coleção</th>
+                    <th>ID Mínimo</th>
+                    <th>ID Máximo</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item) => (
+                    <tr key={item.colecao}>
+                        <td>{item.colecao}</td>
+                        <td>{item.minId}</td>
+                        <td>{item.maxId}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <p>Essa lista é atualizada periodicamente, então fique atento para novas coleções!</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <h2>Usando</h2>
+
+        <p>Para usar essa API, você pode fazer uma requisição GET para o seguinte endpoint:</p>
+        <code>/tradeCard?data=[dados]</code>
+
+        <p>Onde <code>[dados]</code> é um objeto JSON serializado com os seguintes campos:</p>
+        <ul className="list">
+            <li><code>name</code>: Nome do usuário</li>
+            <li><code>icon</code>: ID do ícone do usuário</li>
+            <li><code>id</code>: ID do usuário</li>
+            <li><code>have</code>: Array de cartas que o usuário tem (coleção e id)</li>
+            <li><code>want</code>: Array de cartas que o usuário quer (coleção e id)</li>
+        </ul>
+
+        <p>Exemplo:</p>
+        <code>
+            {`{
+    "name": "John Doe",
+    "icon": "123",
+    "id": "12345",
+    "have": [
+        { "colecao": "0006-FestivalBrilhante", "id": "4" },
+        { "colecao": "0006-FestivalBrilhante", "id": "88" },
+        { "colecao": "0006-FestivalBrilhante", "id": "81" }
+    ],
+    "want": [
+        { "colecao": "0006-FestivalBrilhante", "id": "74" },
+        { "colecao": "0006-FestivalBrilhante", "id": "75" }
+    ]
+}`}
+        </code>
+
+        <p>O JSON precisa ser transformado em base64, em JS pode ser usado <code>window.btoa(JSON.stringify(data)</code>.</p>
+
+        <p>O retorno será uma imagem gerada com as informações do usuário e as cartas que ele tem e quer.</p>
     </div>
-  );
 }
